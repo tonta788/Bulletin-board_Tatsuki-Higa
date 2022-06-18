c4a3d841269b54754751b814105988706ae9bb39
TATSUKI <hetare887@gmail.com>
2022-06-18 19:12:18 +0900

    更新

--- Bulletin-board-master/resources/views/layouts/login.blade.php
+++ Bulletin-board-master/resources/views/layouts/login.blade.php
@@ -21,13 +21,14 @@
     <!--OGPタグ/twitterカード-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <!-- <script src="./js/jquery-3.5.1.min.js"></script> -->
-    <!-- <script src="./js/script.js"></script> -->
+    <script src="./js/jquery-3.5.1.min.js"></script>
+    <script src="./js/script.js"></script>
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/favorite.js"></script>
 </head>
 
 <body>
-    <script src="js/jquery-3.6.0.min.js"></script>
-    <script src="js/favorite.js"></script>
+
 
     <header>
         <div id = "head">
